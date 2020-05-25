7c3b7c2dfb92e96ad60fc9ab81f79f08a8486734
Jonas Wessman Nielsen <5615825@RTS-365.dk>
2020-05-25 11:33:18 +0200

    setup contentful

--- ui-components/gatsby-config.js
+++ ui-components/gatsby-config.js
@@ -1,11 +1,38 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+const contentfulConfig = {
+  spaceId: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+}
+
+// if you want to use the preview API please define
+// CONTENTFUL_HOST in your environment config
+// the `host` property should map to `preview.contentful.com`
+// https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
+if (process.env.CONTENTFUL_HOST) {
+  contentfulConfig.host = process.env.CONTENTFUL_HOST
+}
+
+const { spaceId, accessToken } = contentfulConfig
+
+if (!spaceId || !accessToken) {
+  throw new Error(
+    "Contentful spaceId and the access token need to be provided."
+  )
+}
+
 module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
+  pathPrefix: "/gatsby-contentful-starter",
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -13,8 +40,7 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    "gatsby-plugin-sharp",
     `gatsby-plugin-typescript`,
     {
       resolve: `gatsby-plugin-manifest`,
@@ -28,8 +54,45 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: "gatsby-source-contentful",
+      options: contentfulConfig,
+    },
   ],
 }
+
+// module.exports = {
+//   siteMetadata: {
+//     title: `Gatsby Default Starter`,
+//     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+//     author: `@gatsbyjs`,
+//   },
+//   plugins: [
+//     `gatsby-plugin-react-helmet`,
+//     {
+//       resolve: `gatsby-source-filesystem`,
+//       options: {
+//         name: `images`,
+//         path: `${__dirname}/src/images`,
+//       },
+//     },
+//     `gatsby-transformer-sharp`,
+//     `gatsby-plugin-sharp`,
+//     `gatsby-plugin-typescript`,
+//     {
+//       resolve: `gatsby-plugin-manifest`,
+//       options: {
+//         name: `gatsby-starter-default`,
+//         short_name: `starter`,
+//         start_url: `/`,
+//         background_color: `#663399`,
+//         theme_color: `#663399`,
+//         display: `minimal-ui`,
+//         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+//       },
+//     },
+//     // this (optional) plugin enables Progressive Web App + Offline functionality
+//     // To learn more, visit: https://gatsby.dev/offline
+//     // `gatsby-plugin-offline`,
+//   ],
+// }
